cae343da66fe032e3417ee7991036c98ecd673ed
Eric D. Torres <etorresnotary@gmail.com>
2020-10-15 00:41:36 -0500

    adding JAWSDB_URL environment variable

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql");
+const connection;
 
-const connection = mysql.createConnection({
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);  
+  } else {
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "Louisiana1979!",
     database: "eatDemBurgersDB"
-});
+    })
+  }
 
 connection.connect((err) => {
     if (err) {
