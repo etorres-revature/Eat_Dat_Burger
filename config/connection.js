2cd97be5d2e9800c601ff9171f9584f33c41d615
Eric D. Torres <etorresnotary@gmail.com>
2020-10-17 15:34:53 -0500

    commenting the config folder files (connection and orm)

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,14 @@
+//importing the mysql library
 const mysql = require("mysql");
+
+//creating a variable to hold the connection
 let connection;
 
+//if logic for what to put in the connection variable
 if(process.env.JAWSDB_URL) {
+  //if there is a JAWSDB_URL variable then use that to create the MySQL connection
   connection = mysql.createConnection(process.env.JAWSDB_URL);  
+  //otherwise, use this information to create one on the localhost
   } else {
     connection = mysql.createConnection({
     host: "localhost",
@@ -13,7 +19,7 @@ if(process.env.JAWSDB_URL) {
     })
   }
 
-
+//connect to MySQL
 connection.connect((err) => {
     if (err) {
       console.error(`Error connecting to MySQL: ${err.stack}`);
@@ -22,4 +28,6 @@ connection.connect((err) => {
     console.log(`Connected to MySQL as id ${connection.threadId}`);
   });
 
+
+//export this module
 module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,7 @@
+//importing the connection module for connecting to MySQL
 const connection = require("./connection");
 
+//helper function to convert the number of question marks to a string of that many question marks
 function createMarks(num) {
   let arr = [];
 
@@ -9,36 +11,51 @@ function createMarks(num) {
   return arr.toString();
 }
 
+//helper function to create an array of strings from passed in objects
 function translateSql(obj) {
   let arr = [];
+
+  //loop through the keys and push the key/value as a string into arr variable
   for (let key in obj) {
     let value = obj[key];
+    //check to skip hidden properties
     if (Object.hasOwnProperty.call(obj, key)) {
+      //if string has spaces, include them in the string
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = `"${value}"`;
       }
+      //push the resulting string with the key and value onto arr
       arr.push(`${key}=${value}`);
     }
   }
-  console.log(arr)
+  console.log(arr);
   return arr.toString();
 }
 
+//creating the variable for the object representational mapper (ORM)
 const orm = {
-  
   // selectAll();
   selectAll: (table, cb) => {
+    //query string for this function to pull from model
     const queryString = `SELECT * FROM ${table};`;
+
+    //logging the query string to the console
+    console.log(queryString);
+
+    //calling the MySQL connection to pass the query
     connection.query(queryString, (err, result) => {
+      //error handling
       if (err) {
         throw err;
       }
+      //callback function to return to burger object
       cb(result);
     });
   },
 
   // insertOne();
   insertOne: (table, cols, vals, cb) => {
+    //creating the query string to bring to model
     let queryString = `INSERT INTO ${table}`;
 
     queryString += " (";
@@ -48,18 +65,23 @@ const orm = {
     queryString += createMarks(vals.length);
     queryString += ") ";
 
+    //logging the query string to the console
     console.log(queryString);
 
+    //calling the connection to MySQL to pass the query
     connection.query(queryString, vals, (err, result) => {
+      //error handling
       if (err) {
         throw err;
       }
+      //callback function to return to the burger object
       cb(result);
     });
   },
 
   // updateOne();
   updateOne: (table, objColVals, conditions, cb) => {
+    //creating the query string to bring to the model
     let queryString = `UPDATE ${table}`;
 
     queryString += " SET ";
@@ -67,31 +89,44 @@ const orm = {
     queryString += " WHERE ";
     queryString += conditions;
 
+    //logging the query string to the console
     console.log(queryString);
 
+    //calling the connection to MySQL to pass the query 
     connection.query(queryString, (err, result) => {
+      //error handling
       if (err) {
         throw err;
       }
 
+      //callback function to return to the burger object
       cb(result);
     });
   },
+
   //deleteOn();
   deleteOne: (table, conditions, cb) => {
+    //creating the query string to bring to the model
     let queryString = `DELETE FROM ${table}`;
 
     queryString += " WHERE ";
     queryString += conditions;
 
+    //logging the query string to the console
+    console.log(queryString);
+
+    //calling the connection to MySQL to pass the query
     connection.query(queryString, (err, result) => {
+      //error handling
       if (err) {
         throw err;
       }
 
+      //callback function to return to the burger object
       cb(result);
     });
   },
 };
 
+//exporting the ORM for use in the application
 module.exports = orm;
